f78c5fed10a71a23ca1a2849fe48352e7dd830aa
ry05k2ulv <ryosuke.10117@gmail.com>
2025-03-06 16:13:05 +0900

    chore: Biome と Lefthook の追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import React from "react";
 import ShogiBoard from "./shougi_AI.jsx";
 
 const App = () => {
-  return (
-    <div style={{ 
-      display: "flex", 
-      flexDirection: "column", 
-      justifyContent: "center", 
-      alignItems: "center", 
-      height: "100vh" 
-      
-    }}>
-      <h1>将棋盤</h1>
-      <ShogiBoard />
-    </div>
-  );
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+				height: "100vh",
+			}}
+		>
+			<h1>将棋盤</h1>
+			<ShogiBoard />
+		</div>
+	);
 };
 
 export default App;
