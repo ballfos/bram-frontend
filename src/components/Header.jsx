c7eb78e2714605582047db35a43fe731912e70d8
v-inoue <168613387+v-inoue@users.noreply.github.com>
2025-03-15 08:38:59 +0900

    feat: Header の追加
    * デプロイ設定
    
    * upload-pages-artifactを変更
    
    * deployワークフロー追加
    
    * baseURLの追加
    
    * feat: headerの追加
    
    * chore: git actionsでbuildが通らない問題を修正

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+import { MdContentCopy } from "react-icons/md";
+const Header = ({ onCopySfenClick }) => {
+	return (
+		<header className="header">
+			<h1 className="header-title">Bらm将棋盤</h1>
+			<nav className="header-nav">
+				<ul className="header-list">
+					<li>
+						<a href="#home" className="header-button">
+							<h3>ホーム</h3>
+						</a>
+					</li>
+					<li>
+						<button
+							onClick={onCopySfenClick}
+							className="header-button"
+							type="button"
+						>
+							<MdContentCopy fontSize={24} /> <h3>SFEN</h3>
+						</button>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
